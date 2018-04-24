0017eb90a924b8a395f73224a838418d086fcfdf
sophie <songh2@seattleu.edu>
2018-04-23 20:40:41 -0700

    hw files

---
+++ hw2data.js
@@ -0,0 +1,13 @@
+{
+  "data": [
+    {
+      "Name": "",
+      "designation": "Accountant",
+      "email:mewemail@email.com"
+    },
+    {
+      "name": "Brielle Williamson",
+      "email: hw2@su.edu"
+    }
+  ]
+}
