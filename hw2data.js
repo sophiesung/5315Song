6a50ca41832dadf87621ffa6eb268faf492913f1
sophie <songh2@seattleu.edu>
2018-04-30 19:37:10 -0700

    hw files

--- hw2data.js
+++ hw2data.js
@@ -34,10 +34,27 @@
       "department": "Finance",
       "tel": "3525524252",
       "position": "Associate"
+    },
+    {
+      "name": "Kaitlin Weinber",
+      "email": "kaiw@seattleu.edu",
+      "department": "Operation",
+      "tel": "6619091903",
+      "position": "Associate"
+    },
+    {
+      "name": "Rob Apte",
+      "email": "roba@seattleu.edu",
+      "department": "Engineering",
+      "tel": "3089252932",
+      "position": "Engineer"
+    },
+    {
+      "name": "James Sloane",
+      "email": "js2@seattleu.edu",
+      "department": "Engineering",
+      "tel": "5521391415",
+      "position": "Engineer"
     }
-
-
-
-
   ]
 }
