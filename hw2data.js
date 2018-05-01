83210f5020050d0bc71c219b226c8e6e2a6ba986
sophie <songh2@seattleu.edu>
2018-04-30 19:16:53 -0700

    hw files

--- hw2data.js
+++ hw2data.js
@@ -6,49 +6,6 @@
       "department": "Finance",
       "tel": "2015621322",
       "position": "Associate"
-    },
-    {
-      "name": "Adam Lee",
-      "email": "alee@seattleu.edu",
-      "department": "Accounting",
-      "tel": "2063410062",
-      "position": "Director"
-    },
-    {
-      "name": "Jason Kwon",
-      "email": "jkwon@seattleu.edu",
-      "department": "Supply Chain",
-      "tel": "3089252932",
-      "position": "Supply Planner"
     }
-    // {
-    //   "name": "Thomas Smith",
-    //   "email": "tsmith@seattleu.edu",
-    //   "department": "Engineering",
-    //   "tel": "4079920916",
-    //   "position": "Engineer"
-    // }
-    
-    // {
-    //   "name": "Chloe Simpson",
-    //   "email": "cs2@seattleu.edu",
-    //   "department": "Finance",
-    //   "tel": "3525524252",
-    //   "position": "Associate"
-    // },
-    // {
-    //   "name": "Kaitlin Weinber",
-    //   "email": "kaiw@seattleu.edu",
-    //   "department": "Operation",
-    //   "tel": "6619091903",
-    //   "position": "Associate"
-    // },
-    // {
-    //   "name": "Rob Apte",
-    //   "email": "roba@seattleu.edu",
-    //   "department": "Engineering",
-    //   "tel": "3078222932",
-    //   "position": "Engineer"
-    // }
   ]
 }
