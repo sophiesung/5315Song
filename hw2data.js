e89e611298fce0a6ac5fea19288815f6b1901c87
sophie <songh2@seattleu.edu>
2018-04-30 19:22:43 -0700

    hw files

--- hw2data.js
+++ hw2data.js
@@ -6,6 +6,38 @@
       "department": "Finance",
       "tel": "2015621322",
       "position": "Associate"
+    },
+    {
+      "name": "Adam Lee",
+      "email": "alee@seattleu.edu",
+      "department": "Accounting",
+      "tel": "2063410062",
+      "position": "Director"
+    },
+    {
+      "name": "Jason Kwon",
+      "email": "ajkwon@seattleu.edu",
+      "department": "Supply Chain",
+      "tel": "3089252932",
+      "position": "Supply Planner"
+    },
+    {
+      "name": "Thomas Smith",
+      "email": "asmith@seattleu.edu",
+      "department": "Engineering",
+      "tel": "4079920916",
+      "position": "Engineer"
+    },
+    {
+      "name": "Chloe Simpson",
+      "email": "cs2@seattleu.edu",
+      "department": "Finance",
+      "tel": "3525524252",
+      "position": "Associate"
     }
+
+
+
+
   ]
 }
